ae4c980dd9533a17dbe73a2c0516c530ffeb148c
Samuel <samuel.koesnadi@stud.uni-due.de>
2023-08-26 10:31:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ PYTYPE = ["pytype==2022.7.26"] if IS_NOT_WINDOWS else []
 STABLE_BASELINES3 = "stable-baselines3>=1.7.0,<2.0.0"
 # pinned to 0.21 until https://github.com/DLR-RM/stable-baselines3/pull/780 goes
 # upstream.
-GYM_VERSION_SPECIFIER = "==0.21.0"
+GYM_VERSION_SPECIFIER = "==0.26.2"
 
 # Note: the versions of the test and doc requirements should be tightly pinned to known
 #   working versions to make our CI/CD pipeline as stable as possible.
